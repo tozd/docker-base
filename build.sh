e03fb7faccad96f6c324c4837ff7a758b32ed282
Mitar <mitar.gitlab@tnode.com>
2022-05-07 09:08:29 +0000

    Updating docker-pushrm to 1.8.0.

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 if [ -n "${DOCKER_HUB_PASSWORD}" -a -n "${DOCKER_HUB_USERNAME}" -a "${CI_COMMIT_REF_NAME}" = master ]; then
   echo "${DOCKER_HUB_PASSWORD}" | docker login --username "${DOCKER_HUB_USERNAME}" --password-stdin
   mkdir -p "$HOME/.docker/cli-plugins"
-  wget https://github.com/christian-korneck/docker-pushrm/releases/download/v1.0.1/docker-pushrm_linux_amd64 -O "$HOME/.docker/cli-plugins/docker-pushrm"
+  wget https://github.com/christian-korneck/docker-pushrm/releases/download/v1.8.0/docker-pushrm_linux_amd64 -O "$HOME/.docker/cli-plugins/docker-pushrm"
   chmod +rx "$HOME/.docker/cli-plugins/docker-pushrm"
-  echo "90e9e7a29f14ee215ead57ac51ae307d05e5780c73b031ea75834cf3041012cf  $HOME/.docker/cli-plugins/docker-pushrm" | sha256sum -c -w
+  echo "616339685a474cf5739f4bc5961cdc822e32de2a3c71621a7de32001257da292  $HOME/.docker/cli-plugins/docker-pushrm" | sha256sum -c -w
 fi
 docker build --pull ${BUILD_ARGS} -t "${CI_REGISTRY_IMAGE}:${TAG}" -f "${FILE}" .
 if [ "${CI_COMMIT_REF_NAME}" = master ]; then
