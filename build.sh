a29dc548566c732460b08d55ce43fa20c159960b
Mitar <mitar.git@tnode.com>
2023-05-01 12:31:14 +0200

    Add timing information to docker commands.

--- build.sh
+++ build.sh
@@ -9,17 +9,17 @@ if [ -n "${DOCKER_HUB_PASSWORD}" -a -n "${DOCKER_HUB_USERNAME}" -a "${CI_COMMIT_
   chmod +rx "$HOME/.docker/cli-plugins/docker-pushrm"
   echo "616339685a474cf5739f4bc5961cdc822e32de2a3c71621a7de32001257da292  $HOME/.docker/cli-plugins/docker-pushrm" | sha256sum -c -w
 fi
-docker build --pull ${BUILD_ARGS} -t "${CI_REGISTRY_IMAGE}:${TAG}" -f "${FILE}" .
+time docker build --pull ${BUILD_ARGS} -t "${CI_REGISTRY_IMAGE}:${TAG}" -f "${FILE}" .
 if [ -e test.sh ]; then
   ./test.sh
 fi
 if [ "${CI_COMMIT_REF_NAME}" = master ]; then
-  docker push "${CI_REGISTRY_IMAGE}:${TAG}"
+  time docker push "${CI_REGISTRY_IMAGE}:${TAG}"
 else
-  docker save "${CI_REGISTRY_IMAGE}:${TAG}" | gzip > "${TAG}.tgz"
+  time docker save "${CI_REGISTRY_IMAGE}:${TAG}" | gzip > "${TAG}.tgz"
 fi
 if [ -n "${DOCKER_HUB_PASSWORD}" -a -n "${DOCKER_HUB_USERNAME}" -a "${CI_COMMIT_REF_NAME}" = master ]; then
   docker tag "${CI_REGISTRY_IMAGE}:${TAG}" "tozd/${CI_PROJECT_NAME}:${TAG}"
-  docker push "tozd/${CI_PROJECT_NAME}:${TAG}"
+  time docker push "tozd/${CI_PROJECT_NAME}:${TAG}"
   docker pushrm --debug "tozd/${CI_PROJECT_NAME}"
 fi
